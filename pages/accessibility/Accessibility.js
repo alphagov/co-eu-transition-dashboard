d3a88d440b72a2cb3b4d55dfc0304c9e45be30ab
ahdabnasir@gmail.com <ahdab.nasir@paconsulting.com>
2020-11-18 17:19:55 +0000

    base page

---
+++ pages/accessibility/Accessibility.js
@@ -0,0 +1,18 @@
+const Page = require('core/pages/page');
+const { paths } = require('config');
+const authentication = require('services/authentication');
+
+class Accessibility extends Page {
+  get url() {
+    return paths.accessibility;
+  }
+
+  // Accessibility page is only for static users
+  get middleware() {
+    return [
+    //   ...authentication.protect(['static'])
+    ];
+  }
+}
+
+module.exports = Accessibility;
