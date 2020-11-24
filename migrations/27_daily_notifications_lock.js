abae5697eeb13ddc9b9026a681bb5af7f30488d1
Dominic Taylor <dom.taylor7@gmail.com>
2020-11-24 11:42:46 +0000

    add daily notifications lock

---
+++ migrations/27_daily_notifications_lock.js
@@ -0,0 +1,32 @@
+const { services } = require('config');
+const logger = require('services/logger');
+const Sequelize = require('sequelize');
+const config = require('config');
+
+const up = async (query) => {
+  query.bulkInsert('dashboard_locks', [{
+    name: config.locks.dailyUpdatesNotifications
+  }]);
+};
+
+const down = async (query) => {
+  try {
+    await query.destroy({ where: { name: config.locks.dailyUpdatesNotifications }});
+  } catch (error) {
+    logger.error(`Error rolling back ${error}`);
+  }
+};
+
+module.exports = {
+  up: async (query) => {
+    try {
+      await up(query);
+    } catch (error) {
+      logger.error(`Error migrating ${error}`);
+      logger.error(`Rolling back changes`);
+      await down(query);
+      throw error;
+    }
+  },
+  down
+}
