911b5b15d591a11b47d9201727e7d09ca7cb572e
mateen-hussain <mateen.hussain@yahoo.com>
2020-12-15 18:33:40 +0000

    unit tests

--- helpers/tags.js
+++ helpers/tags.js
@@ -1,7 +1,6 @@
 const TagEntity = require('models/tagEntity');
 const Tag = require('models/tag');
-const sequelize = require("sequelize");
-const Op = sequelize.Op;
+const sequelize = require('services/sequelize');
 
 
 const createEntityTags = async (entityId, tags, transaction) => {
@@ -24,16 +23,17 @@ const removeEntitiesTags = async (entityId, transaction) => {
   }, { transaction });
 }
 
-const createTag = async (name) => {
+const createTag = async (tagName) => {
+  const name = tagName.trim();
   const tagExists = await Tag.findOne({
     where:{
-      name: sequelize.where(sequelize.fn('LOWER', sequelize.col('name')), 'LIKE', name.toLowerCase())
+      name: sequelize.where(sequelize.fn('LOWER', sequelize.col('name')), 'LIKE', name)
     }
   });
   if (tagExists) {
     throw new Error('DUPLICATE_TAG');
   }
-  return Tag.create({name});
+  return Tag.create({ name });
 }
 
 module.exports = {
--- pages/admin/tag-management/add/AddTag.js
+++ pages/admin/tag-management/add/AddTag.js
@@ -2,13 +2,8 @@ const Page = require('core/pages/page');
 const { paths } = require('config');
 const authentication = require('services/authentication');
 const flash = require('middleware/flash');
-const Entity = require('models/entity');
-const EntityFieldEntry = require('models/entityFieldEntry');
-const CategoryField = require('models/categoryField');
 const logger = require('services/logger');
-const config = require('config');
-const uniqBy = require('lodash/uniqBy');
-const {createTag} = require('helpers/tags');
+const tags = require('helpers/tags');
 
 const EMPTY_TAG_ERROR_MESSAGE = "EMPTY_TAG_NAME";
 const DUPLICATE_TAG_ERROR_MESSAGE = "DUPLICATE_TAG";
@@ -30,30 +25,29 @@ class AddTag extends Page {
   }
 
   get successMode() {
-    console.log('***this.req.params', this.req.params);
     return this.req.params && this.req.params.success;
   }
 
   async postRequest(req, res) {
-    let flashMessages; 
+    let flashMessages = [{ text:'something went wrong' }]; 
     try {
       if (!req.body.tagName || req.body.tagName.trim().length === 0) {
         throw new Error(EMPTY_TAG_ERROR_MESSAGE)
       }
-      const tag = await createTag(req.body.tagName);
+      const tag = await tags.createTag(req.body.tagName);
       logger.info(`Tag ${tag.name} created`);
-      console.log('****this.req.originalUrl', this.req.originalUrl);
       return res.redirect(`${this.req.originalUrl}/success`);
     } catch (error) {
       logger.error(error);
+      
       if (error.message && error.message === DUPLICATE_TAG_ERROR_MESSAGE) {
         flashMessages = [{ text:'Similar tag exists, give a different name', href: '#tagName' }];
       } 
+      
       if (error.message && error.message === EMPTY_TAG_ERROR_MESSAGE) {
         flashMessages = [{ text:'Tag cannot be empty', href: '#tagName' }];
-      } else {
-        flashMessages = [{ text:'something went wrong' }];
-      }
+      } 
+      
       req.flash(flashMessages)
       return res.redirect(this.req.originalUrl);
     }
