d5b6c4405ff25e0b837a5f7d1e27804e0b648310
ahdabnasir@gmail.com <ahdab.nasir@paconsulting.com>
2021-01-11 15:40:28 +0000

    entity delete

--- pages/admin/entities/entity-delete/EntityDelete.js
+++ pages/admin/entities/entity-delete/EntityDelete.js
@@ -4,6 +4,8 @@ const authentication = require('services/authentication');
 const Entity = require('models/entity');
 const EntityFieldEntry = require('models/entityFieldEntry');
 const CategoryField = require('models/categoryField');
+const logger = require("services/logger");
+const flash = require('middleware/flash');
 
 class EntityDelete extends Page {
   get url() {
@@ -11,12 +13,17 @@ class EntityDelete extends Page {
   }
 
   get pathToBind() {
-    return `${this.url}/:publicId`;
+    return `${this.url}/:publicId/:successful(successful)?`;
+  }
+
+  get successfulMode() {
+    return this.req.params && this.req.params.successful;
   }
 
   get middleware() {
     return [
-      ...authentication.protect(['admin'])
+      ...authentication.protect(['admin']),
+      flash
     ];
   }
 
@@ -75,8 +82,14 @@ class EntityDelete extends Page {
   }
 
   async postRequest(req, res) {
-    await this.deleteEntity();
-    return res.redirect(paths.admin.entityList);
+    try {
+      await this.deleteEntity();
+    } catch (error) {
+      logger.error(error);
+      req.flash('An error has occurred when deleting this entity');  
+      return res.redirect(this.req.originalUrl);
+    }
+    return res.redirect(`${this.req.originalUrl}/successful`);
   }
 }
 
